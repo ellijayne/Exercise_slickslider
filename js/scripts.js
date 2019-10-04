c2989ef75f888f3aa8c327356791c54c748b7698
ejwillard <ejwillard@squiz.net>
2019-10-04 15:08:39 +1000

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$('.hero-carousel').slick({ 
+
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+        nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+
+    });
+
+
+    var lightbox = $('.hero-gallery a').simpleLightbox();
